5f8a397811621f7050ba71932e519fbeb53cc436
Adrian Antonio Auqui Perez <91244347+Auky216@users.noreply.github.com>
2026-06-27 09:50:59 -0500

    chore: add allowedDevOrigins for server public IP in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  allowedDevOrigins: ["13.140.187.142"],
   async rewrites() {
     const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
     return [
